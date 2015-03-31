3ccab86a1f19a59f10740231c0ea61910e84adc1
Daryle Walker <daryle@Daryle-MacBook-Air.local>
2015-03-31 15:04:39 -0400

    Improve moving downloaded file
    In the download-task completion handler:
        * Switch “if” and “else” cases so the error cases go second.
        * Instead of failing upon the destination file already existing,
    rename the old file while moving the newly downloaded version in place.
    
    Add helper functions to:
        * Generate a unique backup filename for a given file.
        * Move a temporary file to the temporary work directory of the
    volume of another file. (The file replacement API requires both files
    on the same volume.)
    
    Bump up the version and build numbers.

--- cget/cget/main.m
+++ cget/cget/main.m
@@ -23,6 +23,45 @@
 int   returnCode = EXIT_SUCCESS;
 BOOL  shouldExit = NO;
 
+#pragma mark - Support functions
+
+static
+BOOL  CgMoveFileToDestinationTemporaryDirectory(NSURL *sourceTemporaryFile, NSURL *fileOnDestinationVolume, NSURL **newTemporaryFile, NSError **error) {
+    NSCParameterAssert(sourceTemporaryFile);
+    NSCParameterAssert(fileOnDestinationVolume);
+    NSCParameterAssert(newTemporaryFile);
+
+    // Don't move if the source and destination files use the same volume.
+    id  sourceVolume = nil, destinationVolume = nil;
+
+    if ([sourceTemporaryFile getResourceValue:&sourceVolume forKey:NSURLVolumeIdentifierKey error:error] && [fileOnDestinationVolume getResourceValue:&destinationVolume forKey:NSURLVolumeIdentifierKey error:error]) {
+        if ([sourceVolume isEqual:destinationVolume]) {
+            *newTemporaryFile = sourceTemporaryFile;
+            return YES;
+        }
+        // Else: move the source file across volumes, see below.
+    } else {
+        return NO;
+    }
+
+    // Move the source file to the destination file's volume's temporary directory.
+    NSFileManager * const  filer = [NSFileManager defaultManager];
+    NSURL * const        tempDir = [filer URLForDirectory:NSItemReplacementDirectory inDomain:NSUserDomainMask appropriateForURL:fileOnDestinationVolume create:YES error:error];
+
+    if (!tempDir) {
+        return NO;
+    }
+    *newTemporaryFile = [tempDir URLByAppendingPathComponent:sourceTemporaryFile.lastPathComponent];
+    return [filer moveItemAtURL:sourceTemporaryFile toURL:*newTemporaryFile error:error];
+}
+
+static
+NSString *  CgBackupFilename(NSString *originalFilename) {
+    NSCParameterAssert(originalFilename);
+
+    return [[NSString stringWithFormat:@"%@.%@.old", originalFilename.stringByDeletingPathExtension, [NSUUID UUID].UUIDString] stringByAppendingPathExtension:originalFilename.pathExtension];
+}
+
 #pragma mark - Main function
 
 int main(int argc, const char * argv[]) {
@@ -36,21 +75,27 @@ int main(int argc, const char * argv[]) {
         NSRunLoop * const              runLoop = [NSRunLoop currentRunLoop];
         NSURLSession * const           session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration ephemeralSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
         NSURLSessionDownloadTask * const  task = [session downloadTaskWithURL:[NSURL URLWithString:[NSString stringWithUTF8String:argv[1]]] completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
-            if (error) {
-                gbfprint(stderr, @"Error, downloading: %@", error.localizedDescription);
-                gbfprintln(stderr, error.localizedFailureReason ? @" (%@)" : @"", error.localizedFailureReason);
-                returnCode = EXIT_FAILURE;
-            } else {
-                NSURL * const  finalLocation = [NSURL fileURLWithPath:response.suggestedFilename isDirectory:NO];
+            if (!error) {
+                NSCParameterAssert(location);
+                NSCParameterAssert(response);
 
-                [[NSFileManager defaultManager] moveItemAtURL:location toURL:finalLocation error:&error];
-                if (error) {
+                NSURL * const  plannedDestination = [NSURL fileURLWithPath:response.suggestedFilename isDirectory:NO];
+                NSURL *        stagedLocation = nil;
+                NSURL *        actualDestination = nil;
+                
+                NSCAssert(plannedDestination, @"Creating destination URL failed");
+                if (CgMoveFileToDestinationTemporaryDirectory(location, plannedDestination, &stagedLocation, &error) && [[NSFileManager defaultManager] replaceItemAtURL:plannedDestination withItemAtURL:stagedLocation backupItemName:CgBackupFilename(plannedDestination.path.lastPathComponent) options:NSFileManagerItemReplacementUsingNewMetadataOnly | NSFileManagerItemReplacementWithoutDeletingBackupItem resultingItemURL:&actualDestination error:&error]) {
+                    gbprintln(@"%@", actualDestination.path);
+                    // To-do: Is there a way to find out if a backup file was needed and created?
+                } else {
                     gbfprint(stderr, @"Error, copying: %@", error.localizedDescription);
                     gbfprintln(stderr, error.localizedFailureReason ? @" (%@)" : @"", error.localizedFailureReason);
                     returnCode = EXIT_FAILURE;
-                } else {
-                    gbprintln(@"%@", finalLocation.path);
                 }
+            } else {
+                gbfprint(stderr, @"Error, downloading: %@", error.localizedDescription);
+                gbfprintln(stderr, error.localizedFailureReason ? @" (%@)" : @"", error.localizedFailureReason);
+                returnCode = EXIT_FAILURE;
             }
             shouldExit = YES;
         }];
